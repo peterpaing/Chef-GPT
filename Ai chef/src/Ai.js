67860df3f355b51c0f00a01b2890c4a4f973b4c1
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-08 11:17:31 +0630

    connect with Ai api

---
+++ Ai chef/src/Ai.js
@@ -0,0 +1,24 @@
+const ai = 'https://hook.eu1.make.com/g25dob4z7uq11wrjnp31ruiiwy3yuo4p';
+
+export async function getAiResponse(messageText) {
+    try {
+        const res = await fetch(ai, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user_prompt: messageText 
+            })
+        });
+
+        const aiReply = await res.text();
+        console.log("AI Answer:", aiReply);
+        return aiReply;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+window.getAiResponse = getAiResponse
--- Ai chef/src/App.jsx
+++ Ai chef/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
+import { getAiResponse } from './Ai';
 import Header from './components/HeaderSection'
 import Main from './components/MainSection'
+
+
+getAiResponse("Where is myanmar")
 
 export default function App(){
     return (
